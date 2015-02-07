aa5762fb27ed3e73c6f02d802f4b9bbf0f38692d
david63 <david.wood@pear-tree.org.uk>
2015-02-07 09:44:41 +0000

    Language updates

--- david63/sitelogo/language/sv/info_acp_sitelogo.php
+++ david63/sitelogo/language/sv/info_acp_sitelogo.php
@@ -34,33 +34,43 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'SITE_LOGO'					=> 'Egen logga',
+	'LOGO_CENTRE'					=> 'Centrerat',
+	'LOGO_LEFT'						=> 'Vänster',
+	'LOGO_RIGHT'					=> 'Höger',
 
-	'SITE_LOGO_EXPLAIN'			=> 'Här kan du ändra inställningarna för att visa din egna sidlogga som ska ersätta standardloggan.',
+	'SITE_LOGO'						=> 'Egen logga',
 
-	'SITE_LOGO_HEIGHT'			=> 'Loggans höjd',
-	'SITE_LOGO_HEIGHT_EXPLAIN'	=> 'Lämnas detta fält tomt så används standardloggans höjd (152px).',
+	'SITE_LOGO_EXPLAIN'				=> 'Här kan du ändra inställningarna för att visa din egna sidlogga som ska ersätta standardloggan.',
 
-	'SITE_LOGO_IMAGE'			=> 'Sökväg till den egna sidloggan.',
-	'SITE_LOGO_IMAGE_EXPLAIN'	=> 'Lämna detta fält tomt för att använda standardloggan.<br />Om du vill använda en logga från en annan domän anger du hela URLen, annars anger du den lokala sökvägen.',
+	'SITE_LOGO_HEIGHT'				=> 'Loggans höjd',
+	'SITE_LOGO_HEIGHT_EXPLAIN'		=> 'Lämnas detta fält tomt så används standardloggans höjd (152px).',
 
-	'SITE_LOGO_LEFT'			=> 'Vänstra hörn',
-	'SITE_LOGO_LEFT_EXPLAIN'	=> 'Runda av de vänstra hörnen för att matcha sidhuvudet.',
-	'SITE_LOGO_LOG'				=> '<strong>Inställningarna för den egna loggan har sparats</strong>',
+	'SITE_LOGO_IMAGE'				=> 'Sökväg till den egna sidloggan.',
+	'SITE_LOGO_IMAGE_EXPLAIN'		=> 'Lämna detta fält tomt för att använda standardloggan.<br />Om du vill använda en logga från en annan domän anger du hela URLen, annars anger du den lokala sökvägen.',
 
-	'SITE_LOGO_MANAGE'			=> 'Hantera egen logga',
+	'SITE_LOGO_LEFT'				=> 'Vänstra hörn',
+	'SITE_LOGO_LEFT_EXPLAIN'		=> 'Runda av de vänstra hörnen för att matcha sidhuvudet.',
+	'SITE_LOGO_LOG'					=> '<strong>Inställningarna för den egna loggan har sparats</strong>',
 
-	'SITE_LOGO_OPTIONS'			=> 'Inställningar för egen logga',
+	'SITE_LOGO_MANAGE'				=> 'Hantera egen logga',
 
-	'SITE_LOGO_PIXELS'			=> 'pixlar',
-	'SITE_LOGO_PIXELS_EXPLAIN'	=> 'Ställer in pixlarna för hörnets rundning.',
+	'SITE_LOGO_OPTIONS'				=> 'Inställningar för egen logga',
 
-	'SITE_LOGO_RIGHT'			=> 'Högra hörn',
-	'SITE_LOGO_RIGHT_EXPLAIN'	=> 'Runda av de högra hörnen för att matcha sidhuvudet.',
+	'SITE_LOGO_PIXELS'				=> 'pixlar',
+	'SITE_LOGO_PIXELS_EXPLAIN'		=> 'Ställer in pixlarna för hörnets rundning.',
+	'SITE_LOGO_POSITION'			=> 'Loggans position',
 
-	'SITE_LOGO_SUPRESS'			=> 'Dölj texterna intill loggan',
-	'SITE_LOGO_SUPRESS_EXPLAIN'	=> 'Denna inställning döljer texterna <strong>Site Name</strong> och <strong>Site Description</strong>.',
+	'SITE_LOGO_REMOVE'				=> 'Dölj loggan',
+	'SITE_LOGO_REMOVE_EXPLAIN'		=> 'Denna inställning döljer sidans logga.',
+	'SITE_LOGO_RIGHT'				=> 'Högra hörn',
+	'SITE_LOGO_RIGHT_EXPLAIN'		=> 'Runda av de högra hörnen för att matcha sidhuvudet.',
 
-	'SITE_LOGO_WIDTH'			=> 'Loggans bredd',
-	'SITE_LOGO_WIDTH_EXPLAIN'	=> 'Lämnas detta fält tomt så används standardloggans bredd (149px).',
+	'SITE_LOGO_WIDTH'				=> 'Loggans bredd',
+	'SITE_LOGO_WIDTH_EXPLAIN'		=> 'Lämnas detta fält tomt så används standardloggans bredd (149px).',
+
+	'SITE_NAME_SUPRESS'				=> 'Dölj texterna intill loggan',
+	'SITE_NAME_SUPRESS_EXPLAIN'		=> 'Denna inställning döljer texterna <strong>Site Name</strong> och <strong>Site Description</strong>.',
+
+	'SITE_SEARCH_REMOVE'			=> 'Dölj sökrutan',
+	'SITE_SEARCH_REMOVE_EXPLAIN'	=> 'Denna inställning döljer sökrutan i sidhuvudet.',
 ));
